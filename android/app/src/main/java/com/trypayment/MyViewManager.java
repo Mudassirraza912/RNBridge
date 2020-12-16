941bda5db403252b276d986566d5e1703afee879
Mudassirraza912 <razamudassir912@gmail.com>
2020-12-16 12:24:16 +0500

    show btn xm

--- android/app/src/main/java/com/trypayment/MyViewManager.java
+++ android/app/src/main/java/com/trypayment/MyViewManager.java
@@ -1,9 +1,12 @@
 package com.trypayment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -19,11 +22,13 @@ import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.paymentez.android.Paymentez;
+import com.paymentez.android.model.Card;
+import com.paymentez.android.rest.TokenCallback;
 import com.paymentez.android.view.CardMultilineWidget;
 import java.util.Map;
 
 public class MyViewManager extends SimpleViewManager<LinearLayout> {
-
+    Button btn;
     public static final String REACT_NAME="MyViewManager";
 //    public int COMMAND_CREATE=1;
     ReactApplicationContext reactContext;
@@ -50,7 +55,25 @@ public class MyViewManager extends SimpleViewManager<LinearLayout> {
 //            }
 //        });
        // Paymentez.setEnvironment(true, Constan,"");
-        return linearLayout; //If your customview has more constructor parameters pass it from here.
+       // Log.d("childCount",""+linearLayout.getChildCount());
+        linearLayout.getChildAt(0);
+        btn=linearLayout.findViewById(R.id.btn);
+        CardMultilineWidget card= (CardMultilineWidget) linearLayout.getChildAt(0);
+        Card cardToSave = card.getCard();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cardToSave == null) {
+                    Log.d("test" , " if");
+
+                    //return linearLayout; //If your customview has more constructor parameters pass it from here.
+                }else{
+                    Log.d("test" , card+" else");
+                }
+            }
+        });
+
+        return linearLayout;
     }
 
 }
